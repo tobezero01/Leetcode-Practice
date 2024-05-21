beaa7d2f371c58721e0a637e073c7fe380ad4325
tobezero01 <ducnhuad@gmail.com>
2024-05-21 17:33:21 +0700

    init commit 2

--- src/Arrays/mainArrays.java
+++ src/Arrays/mainArrays.java
@@ -1,10 +1,36 @@
 package Arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// leetcode arrays
 public class mainArrays {
 
-
+    //448
+//    public static List<Integer> findDisappearedNumbers(int[] nums) {
+//        // Duyệt qua mỗi phần tử trong mảng
+//        for (int i = 0; i < nums.length; i++) {
+//            // Lấy chỉ số (giá trị của phần tử - 1)
+//            int index = Math.abs(nums[i]) - 1;
+//            // Đánh dấu phần tử tại vị trí chỉ số này (bằng cách đổi dấu)
+//            if (nums[index] > 0) {
+//                nums[index] = -nums[index];
+//            }
+//        }
+//
+//        // Tạo danh sách để lưu các giá trị không xuất hiện
+//        List<Integer> result = new ArrayList<>();
+//        // Duyệt lại mảng để tìm các giá trị không xuất hiện
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] > 0) {
+//                result.add(i + 1);
+//            }
+//        }
+//
+//        return result;
+//    }
 
     public static void main(String[] args) {
-
+        
     }
 }
